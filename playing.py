4a4fbf9b003ae63565b3e772877ccff6b89b36e5
Linhárt Ervin <LinhartE@olajterv.hu>
2019-12-16 14:07:40 +0100

    playing with srings

---
+++ playing.py
@@ -0,0 +1,14 @@
+message_1 = "Hello"
+message_2 = "and"
+message_3 = "welcome"
+
+message_4 = message_3.count("e")
+
+append = f'{message_1.lower()} {message_2.upper()} {message_3[1:3]} to the world!'
+
+#print(append)
+
+#print(help(str.count))
+
+print(message_4)
+
