34bf1f5372fd07007b020a04d8221cec49b9c247
Linhárt Ervin <LinhartE@olajterv.hu>
2020-03-24 07:53:16 +0100

    Pong final

--- Pong.py
+++ Pong.py
@@ -1,11 +1,13 @@
 # Simple Pong in Python 3 for Beginners
 # By @TokyoEdTech
-# Part 6: Scoring
+# Part 7: Sound
 
 import turtle
+import winsound
+import time
 
 wn = turtle.Screen()
-wn.title("Pong by @TokyoEdTech")
+wn.title("Pong by @TokyoEdTech Modified by Ervin")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
@@ -18,7 +20,7 @@ score_b = 0
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("white")
+paddle_a.color("red")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
@@ -27,49 +29,49 @@ paddle_a.goto(-350, 0)
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("white")
+paddle_b.color("blue")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(0.1)
-ball.shape("square")
-ball.color("white")
+ball.speed(0)
+ball.shape("circle")
+ball.color("freen")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.15
-ball.dy = 0.15
+ball.dx = 2
+ball.dy = 2
 
 # Pen
 pen = turtle.Turtle()
 pen.speed(0)
-pen.color("white")
+pen.color("yellow")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "bold"))
 
 # Function
 def paddle_a_up():
 	y = paddle_a.ycor()
-	y += 20
+	y += 40
 	paddle_a.sety(y)
 
 def paddle_a_down():
 	y = paddle_a.ycor()
-	y -= 20
+	y -= 40
 	paddle_a.sety(y)
 
 def paddle_b_up():
 	y = paddle_b.ycor()
-	y += 20
+	y += 40
 	paddle_b.sety(y)
 
 def paddle_b_down():
 	y = paddle_b.ycor()
-	y -= 20
+	y -= 40
 	paddle_b.sety(y)
 
 # Keyboard binding
@@ -83,6 +85,8 @@ wn.onkeypress(paddle_b_down, "Down")
 while True:
     wn.update()	
 
+    time.sleep(0.005)
+
 	# Move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
@@ -91,31 +95,35 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "bold"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "bold"))
 
 
     # Paddle and ball collisions
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
-        ball.setx(340)
+    if (ball.xcor() > 330 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50):
+        ball.setx(330)
         ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
-        ball.setx(-340)
+    if (ball.xcor() < -330 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50):
+        ball.setx(-330)
         ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
